da187bb8d929288fea7e69416589ff6c880c7968
joshuajomiller <joshuajosephmiller@gmail.com>
2018-04-16 23:27:30 +0300

    User create check existing

--- treat-server/routes/users.js
+++ treat-server/routes/users.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router();
 const User = require("../models/User");
+const jwt  = require('jsonwebtoken');
 
 /* GET users listing. */
 
@@ -10,13 +11,27 @@ router.route('/')
     res.send('Get all Users')
   })
   .post(function (req, res) {
-    User.create({ email: req.body.email, password: req.body.password }, function (err, small) {
-      if (err){
-        res.status(400).send(err)
-      }
-      res.send('Saved!');
-    });
+    let email = req.body.email;
+    let password = req.body.password;
 
+    User.findOne({email: email}, (err, existingUser) => {
+      if (err) {
+        rres.status(400).send({error: err});
+      } else {
+        if (existingUser) {
+          res.status(400).send({msg: 'There is already an existing user registered with that email.'});
+        } else {
+          User.create({email: email, password: password}, function (err, small) {
+            if (err) {
+              res.status(400).send(err)
+            }
+            const token = jwt.sign({email: email, password: password}, 'your_jwt_secret_123');
+            console.log(token);
+            res.send({token});
+          });
+        }
+      }
+    });
   });
 
 /* GET user profile. */
