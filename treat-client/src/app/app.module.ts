5770b47f5fbb342f3c4b858695c3d98907197125
Joshua Miller <joshua_m@mer-group.com>
2018-05-07 19:12:25 +0300

    Added package.json + http client

--- treat-client/src/app/app.module.ts
+++ treat-client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     NgbModule.forRoot()
   ],
   providers: [],
