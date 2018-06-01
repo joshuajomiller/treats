6a22ce6137bd02062cc3cae7b90b6702a4fc0ef6
joshuajomiller <joshuajosephmiller@gmail.com>
2018-06-01 17:11:19 +0300

    New board component

---
+++ treat-client/src/app/new-board/new-board.component.ts
@@ -0,0 +1,25 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+
+
+@Component({
+  selector: 'app-new-board',
+  templateUrl: './new-board.component.html',
+  styleUrls: ['./new-board.component.scss']
+})
+export class NewBoardComponent implements OnInit {
+
+  @Output() action = new EventEmitter();
+
+  public boardName: string;
+
+  constructor(public activeModal: NgbActiveModal) { }
+
+  ngOnInit() {
+  }
+
+  closeModal() {
+    this.action.emit(this.boardName);
+    this.activeModal.close();
+  }
+}
