21b61991a8d15b8cc4e8c9b30f44e1086ef08177
joshuajomiller <joshuajosephmiller@gmail.com>
2018-10-16 23:36:52 +0300

    Continue share board component

--- treat-client/src/app/share-board/share-board.component.ts
+++ treat-client/src/app/share-board/share-board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -8,9 +8,17 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 })
 export class ShareBoardComponent implements OnInit {
 
+  public emailAddress: string;
+
+  @Output() action = new EventEmitter();
+
   constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit() {
+  }
+
+  addUser() {
+    this.action.emit({email: this.emailAddress});
   }
 
   done() {
